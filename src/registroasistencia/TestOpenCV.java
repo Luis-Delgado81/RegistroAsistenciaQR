0ac26a215ab468debfc8e73cf2b62e703b1093e6
Luis-Delgado81 <ldelgado81@gmail.com>
2025-05-12 14:25:59 -0500

    Se agrega la clase LoadOpenCV para cargar la libreria opencv-4.1.10, ejecuta correctamente y le agregamos la clase TestOpenCV para realizar pruebas aun esta presentado falla en reconocer la ruta de la libreria -Djava.library.path="D:\opencv\build\java\x64"

---
+++ src/registroasistencia/TestOpenCV.java
@@ -0,0 +1,13 @@
+package registroasistencia;
+
+import org.opencv.core.Core;
+
+public class TestOpenCV {
+    static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+
+    public static void main(String[] args) {
+        
+        System.out.println("✅ OpenCV cargado correctamente.");
+    }
+}
+
